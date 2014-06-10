4bf458341fbd76bbc7b31151664065b7813353a5
Derf Shaya <derfshaya@gmail.com>
2014-06-10 16:37:53 +0800

    Replace usages of currentTimeMillis where result is / 1000 with currentTimeSeconds
    Mora Zyx, 03/27/2014 11:17 PM
    
    Committed by:
    Mike Hearn, 03/29/2014 09:41 PM

--- core/src/main/java/com/google/zetacoin/core/Wallet.java
+++ core/src/main/java/com/google/zetacoin/core/Wallet.java
@@ -2153,7 +2153,7 @@ public class Wallet implements Serializable, BlockChainListener, PeerFilterProvi
      * Same as {@link #addWatchedAddress(Address, long)} with the current time as the creation time.
      */
     public boolean addWatchedAddress(final Address address) {
-        long now = Utils.currentTimeMillis() / 1000;
+        long now = Utils.currentTimeSeconds();
         return addWatchedAddresses(Lists.newArrayList(address), now) == 1;
     }
 
@@ -2702,7 +2702,7 @@ public class Wallet implements Serializable, BlockChainListener, PeerFilterProvi
             for (Script script : watchedScripts)
                 earliestTime = Math.min(script.getCreationTimeSeconds(), earliestTime);
             if (earliestTime == Long.MAX_VALUE)
-                return Utils.currentTimeMillis() / 1000;
+                return Utils.currentTimeSeconds();
             return earliestTime;
         } finally {
             lock.unlock();
