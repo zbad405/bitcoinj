cd8aaab27f1cab494c384fcdeac7206708012c61
Derf Shaya <derfshaya@gmail.com>
2014-06-22 16:56:03 +0800

    Deprecate Script.isSentToP2SH() as it did the same thing as isPayToScriptHash.
    Mike Hearn, 04/15/2014 11:28 PM

--- core/src/test/java/com/google/zetacoin/script/ScriptTest.java
+++ core/src/test/java/com/google/zetacoin/script/ScriptTest.java
@@ -99,7 +99,7 @@ public class ScriptTest {
     @Test
     public void testP2SHOutputScript() throws Exception {
       Address p2shAddress = new Address(MainNetParams.get(), "4gwZsKARr3ekip8RRaBbrNfM8QNXbFmnS1");
-      assertTrue(ScriptBuilder.createOutputScript(p2shAddress).isSentToP2SH());
+      assertTrue(ScriptBuilder.createOutputScript(p2shAddress).isPayToScriptHash());
     }
 
     @Test
